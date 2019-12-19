7d10c3a170ceb7921f68dbdd570b7262bfecd4e7
Moshe Stern <moshecstern@gmail.com>
2019-12-19 11:26:21 -0500

    changed logic for smoother running of app

--- src/App.js
+++ src/App.js
@@ -6,106 +6,161 @@ import SuperheroCard from "./components/SuperheroCard";
 import Navbar from "./components/Navbar";
 import Superheros from "./superheros.json";
 
-
+let counter = 0;
 
 class App extends React.Component {
   state = {
     Score: 0,
     HighScore: 0,
-    // Clicked: 0,
-    // NewSuperheros: [],
+    CounterState: 0,
+    NewSuperheros: [],
     Superheros: Superheros
   }
 
-  ClickedImage = (id, Clicked, fakeid) => {
-        // const NewSuperheros = this.state.Superheros;
-    const NewSuperheros = this.state.Superheros.map(pic => {
-      if (pic.id === id) {
-        if(!pic.clicked) {
-          pic.clicked = true
+  
 
-          this.setState({
-            Score: this.state.Score+1
-          });
+  ClickedImage = id => {
+    const { NewSuperheros } = this.state;
+    // console.log(id)
 
+    this.shuffle();
+    if (NewSuperheros.includes(id)) {
+      this.setState({Score: 0})
+      
+      //logic for couter
+      // this.setState({ HighScore: this.state.Score})
+      // this.setState({ Score: })
+    }
+    else if (!NewSuperheros.includes(id)) {
+      counter = counter += 1;
+      console.log(counter);
+      // this.state({ CounterState: counter})
+      // this.setState({ HighScore: counter})
+      this.setState({ Score: counter})
+      if(this.state.Score > this.state.HighScore){
+        this.setState({HighScore: this.state.Score})
+      }
+      
+      
+      NewSuperheros.push(id);
+      //logic for counter
+    }
+    // const uniqueNames = names.filter((val, id, array) => {
+    //   return array.indexOf(val) == id;  
+    //  });
+    // const NewSuperheros = this.state.Superheros;
+    // const NewSuperheros = this.state.Superheros.map(pic => {
+    //   if (pic.id === id) {
+    //     if (!pic.clicked) {
+    //       pic.clicked = true
+    //       this.updateScore(true)
+    //       this.UpdateHighScore();
+
+          // this.shuffle()
+          // this.setState({
+          //   Score: this.state.Score+1
+          // });
           // update the top score if score is greater than top score
-
           // if the score is equal to 12 you win!!!
           // then reset game but do not reset top score
-        } else {
-          alert("You lose!");
+        // } else {
           // Now reset the game
-          this.setState({
-            Score: 0,
-            Superheros: Superheros
-          });
-        }
-      }
+          // pic.clicked = false
 
-      return pic;
-    });
+          // this.updateScore(false)
+          // this.UpdateHighScore()
+          // this.resetGame(true);
+          // return !pic;
 
-    this.setState({
-      Superheros: NewSuperheros
-    })
-    
-        // console.log(ClickedSupers);
-        // // this.setState({
-        // //   Superheros: NewSuperheros
-        // // })
-        // console.log("id")
-        // console.log(id);
-        // console.log("NewSuperheros")
-        // console.log(NewSuperheros);
-        // 
-        // console.log(item.id);
-        // if clicked allready then game over, save high score and restart game
-        // else this image gets Clicked: 1(true) and shuffle board/ randomize the new array of superheros 
-        // if (this.state.NewSuperheros.includes(ClickedSupers)) {
-        //   // if (this.state.NewSuperheros.includes(name)){
-    
-        //   // }
-        //   console.log("if this.state.Clicked is 0")
-        //   this.setState({
-        //     // Clicked: true,
-        //     // })
-        //     // //set the state of friends to be the new array
-        //     // this.setState({
-    
-        //     // arr: this.state.arr.concat('new value')
-    
-        //     // NewSuperheros: this.Superheros,
-        //     // ClickedImage: true,
-        //     Score: this.state.Score + 1
-        //   })
-        //   console.log("id")
-        //   console.log(id);
-        //   console.log("this.state.score")
-        //   console.log(this.state.Score);
-        //   console.log("this.state.Clicked")
-        //   console.log(this.state.Clicked)
-        //   // console.log("Clicked")
-        //   // console.log(Clicked);
-        //   // shuffleboard
-        //   // UpdateHighScore(this.state.Score);
-        //   // updatehighscore function
-        //   //  UpdateHighScore = (Score) => {
-        //   //  if(this.state.Score > this.state.HighScore){
-        //   //  return this.setState({
-        //   //    HighScore: this.state.Score
-        //   //  })
-        //   // this.state.HighScore = this.state.Score;
-        //   // this.state.Score= 0
-    
-        //   //  }
-        //   //     }
-        //   //  } else  {
-        //   console.log("youv clicked this allready, try again!")
-        // }
-    
-    
-      }
-    
+          // this.setState({
+          //   // Score: 0,
+          //   Superheros: Superheros
+          // });
+          // alert("You lose!");
+    //     }
+    //   }
+    //   console.log("********")
+    //   console.log(pic);
+    //   console.log("********")
+    //   return pic;
+
+    // });
+
+    // if (pic.id === id){
+
+    // this.setState({
+    //   Superheros: NewSuperheros
+    // })
+    // console.log("adding/ setting NewSuperheros to Superheros; line62")
+    // // }else{
+    //   this.setState({
+    //     Superheros: Superheros
+    //   })
+    // }
+
+  }
+  // end of on click image
+
+  // updatehighscore function
+  // UpdateHighScore = () => {
+  //   if (this.state.Score === 12) {
+  //     alert("You win");
+  //     // reset game function
+  //     this.resetGame(false)
+  //   } else if (this.state.Score > this.state.HighScore) {
+  //     this.setState({
+  //       HighScore: this.state.Score
+  //     })
+  //     // this.resetGame(false)
+  //   }
+  // }
+  // updateScore = (boolean) => {
+  //   if (boolean === true) {
+  //     return this.setState({
+  //       Score: this.state.Score + 1,
+  //     });
+  //   } else if (boolean === false) {
+  //     return this.setState({
+  //       Score: 0,
+  //     })
+  //   }
+  // } // end of updatescore func
+
+  //shuffle board function
+  shuffle = () => {
+    this.state.Superheros.sort(function (a, b) { return 0.5 - Math.random() });
+    // this.state.Superheros.sort(() => Math.random() - 3.33)
+  }
+  //reset game function
+  resetGame = boolean => {
+    if (boolean === true) {
+      // reset entire game and set score to 0 and call state superheros: superheros
+      // shuffleboard
+      // this.state.Superheros.forEach(pic => {
+      //   pic.clicked = false;
+      // });
+      this.shuffle()
+      alert("You lose!")
+      this.setState({
+        Score: 0,
+        Superheros: []
+      })
+      return this.setState({
+        Superheros: Superheros
+      })
+      // console.log("making Superheros: superheros; line 124")
+    } else {
+      // shuffle board
+      // this.setState({
+      //   Superheros: Superheros
+      // })
+      // console.log("making Superheros: superheros; line 130")
+
+      this.shuffle()
+    }
+  }
+
+
 
   render() {
 
@@ -115,17 +170,17 @@ class App extends React.Component {
         <Wrapper>
 
 
-        {this.state.Superheros.map((item) =>
-          // {this.state.NewSuperheros.map((item, id) =>
+          {this.state.Superheros.map((item) => (
+            // {this.state.NewSuperheros.map((item, id) =>
 
             <SuperheroCard
               key={item.id}
               id={item.id}
               name={item.name}
               image={item.image}
-              ClickedImage={this.ClickedImage}
+              ClickedImage={() => this.ClickedImage(item.id)}
             />
-          )}
+          ))}
 
         </Wrapper>
       </div>
@@ -135,127 +190,3 @@ class App extends React.Component {
 
 export default App;
 
-// ORIGINIAL IS ABOVE
-
-
-
-// import React from 'react';
-// // import logo from './logo.svg';
-// import './App.css';
-// import Wrapper from "./components/Wrapper";
-// import SuperheroCard from "./components/SuperheroCard";
-// import Navbar from "./components/Navbar";
-// import Superheros from "./superheros.json";
-
-
-// class App extends React.Component {
-//   state = {
-//     Score: 0,
-//     HighScore: 0,
-//     maxScore: 12,
-//     // isGuessCorrect: true,
-//     // Clicked: 0,
-//     NewSuperheros: [],
-//     Superheros: Superheros
-//   }
-
-//   // can i have seprate array with the same items as superhero array and remove 
-//   // game start =s by clicking img
-//   ClickedImage = (id, Clicked, fakeid) => {
-//     const NewSuperheros = this.state.Superheros;
-//     const ClickedSupers = NewSuperheros.map(pic => {
-//       if (pic.id === id) {
-//         pic.clicked = true
-//       }
-
-//       return pic;
-//     });
-
-//     console.log(ClickedSupers);
-//     // this.setState({
-//     //   Superheros: NewSuperheros
-//     // })
-//     console.log("id")
-//     console.log(id);
-//     console.log("NewSuperheros")
-//     console.log(NewSuperheros);
-//     console.log("Clicked this.state");
-//     console.log(this.state.Clicked);
-//     console.log("superhero.clicked");
-//     console.log(Clicked)
-//     // console.log(item.id);
-//     // if clicked allready then game over, save high score and restart game
-//     // else this image gets Clicked: 1(true) and shuffle board/ randomize the new array of superheros 
-//     if (this.state.NewSuperheros.includes(ClickedSupers)) {
-//       // if (this.state.NewSuperheros.includes(name)){
-
-//       // }
-//       console.log("if this.state.Clicked is 0")
-//       this.setState({
-//         Clicked: true,
-//         // })
-//         // //set the state of friends to be the new array
-//         // this.setState({
-
-//         // arr: this.state.arr.concat('new value')
-
-//         // NewSuperheros: this.Superheros,
-//         // ClickedImage: true,
-//         Score: this.state.Score + 1
-//       })
-//       console.log("id")
-//       console.log(id);
-//       console.log("this.state.score")
-//       console.log(this.state.Score);
-//       console.log("this.state.Clicked")
-//       console.log(this.state.Clicked)
-//       // console.log("Clicked")
-//       // console.log(Clicked);
-//       // shuffleboard
-//       // UpdateHighScore(this.state.Score);
-//       // updatehighscore function
-//       //  UpdateHighScore = (Score) => {
-//       //  if(this.state.Score > this.state.HighScore){
-//       //  return this.setState({
-//       //    HighScore: this.state.Score
-//       //  })
-//       // this.state.HighScore = this.state.Score;
-//       // this.state.Score= 0
-
-//       //  }
-//       //     }
-//       //  } else  {
-//       console.log("youv clicked this allready, try again!")
-//     }
-
-
-//   }
-
-//   render() {
-
-//     return (
-//       <div>
-//         <Navbar count={this.state.Score} highcount={this.state.HighScore} />
-//         <Wrapper>
-
-
-//           {/* {Superheros.map((item, id, clicked) => */}
-//           {this.state.NewSuperheros.map((item, id, clicked) =>
-
-//             <SuperheroCard
-//               key={id}
-//               id={item.id}
-//               name={item.name}
-//               image={item.image}
-//               Clicked={clicked}
-//               ClickedImage={this.ClickedImage}
-//             />
-//           )}
-
-//         </Wrapper>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
