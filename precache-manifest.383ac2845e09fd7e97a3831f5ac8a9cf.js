86acad4d1cf8771349d9eb1f286fc5fa9ab10f26
Moshe Stern <moshecstern@gmail.com>
2019-12-19 14:42:37 -0500

    Updates

---
+++ precache-manifest.383ac2845e09fd7e97a3831f5ac8a9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dc3a3645f2c0635f4814514d9d8189",
+    "url": "/reactmemorygame/index.html"
+  },
+  {
+    "revision": "f7505dbabc53974b06ed",
+    "url": "/reactmemorygame/static/css/main.ada4faf8.chunk.css"
+  },
+  {
+    "revision": "e1698639a58e2c7f189d",
+    "url": "/reactmemorygame/static/js/2.244a274e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactmemorygame/static/js/2.244a274e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7505dbabc53974b06ed",
+    "url": "/reactmemorygame/static/js/main.a3b785e5.chunk.js"
+  },
+  {
+    "revision": "1791a6fb820583e028b5",
+    "url": "/reactmemorygame/static/js/runtime-main.f4801a4f.js"
+  }
+]);
